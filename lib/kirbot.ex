e411c1e0d5ac92ab5f3ccd80d5b5a1d61065c031
cronokirby <cronokirby@gmail.com>
2017-04-21 20:46:03 +0200

    basic shell

--- lib/kirbot.ex
+++ lib/kirbot.ex
@@ -2,9 +2,18 @@ defmodule Kirbot do
   @moduledoc """
   Entry module to the bot application.
   """
+  use Application
+  alias Alchemy.Client
 
   @token Application.get_env(:kirbot, :token)
-  def hello do
-    :world
+
+  def start(_type, _args) do
+    run = Client.start(@token)
+    load_modules()
+    run
+  end
+
+  def load_modules do
+    # modules will be added here later
   end
 end
